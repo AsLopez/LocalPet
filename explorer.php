26e9f8199adcd9a0420084f1fb9bbd56e459ee98
Anderson Lopez <and3.s@hotmail.com>
2018-04-25 23:41:00 -0500

    Página explorador de usuarios

---
+++ explorer.php
@@ -0,0 +1,72 @@
+<?php 
+	session_start();
+	require 'database.php';
+/*
+	if (isset($_SESSION['user_id'])) {
+		header('Location: /web/Petry/login.php');
+	}
+*/
+	if (isset($_SESSION['user_id'])) {
+		$records = $conn->prepare('SELECT * FROM adopciones');
+		$records->execute();
+		$results = $records->fetch(PDO::FETCH_ASSOC);
+		
+		$user = null;
+
+		if (count($results)>0) {
+			$user = $results;
+		}
+	}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EXPLORADOR</title>
+	<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+</head>
+<body>
+	<?php require 'parciales/header.php' ?>
+
+	<center>
+		<div class="publicaciones">
+			<table style="width:100%">
+				<tr>
+					    <th>Numero de adopcion</th>
+					    <th>Usuario adoptante</th> 
+					    <th>Adopto</th>
+					    <th align="center">Mas información</th>
+				</tr>
+			<?php
+			$consulta = $conn->prepare('SELECT id_adopcion,usuario_id,publicacion_id FROM adopciones');
+			$consulta->execute();
+			?>
+			<?php while ($fila = $consulta->fetch(PDO::FETCH_ASSOC)) { ?>
+				<div>
+					  <tr>
+					  	<td><?php echo $fila['id_adopcion']; ?></td>
+					  	<td><?php echo $fila['usuario_id']; ?></td>
+					  	<td><?php echo $fila['publicacion_id']; ?></td>
+					  	<td align="center"><input type="submit" value="+"></td>
+				</div>
+			<?php } ?>
+			<?php if (!empty($user))
+						{
+							$sql = "INSERT INTO adopciones (usuario_id,publicacion_id) VALUES (:usuario_id,:publicacion_id)";
+							$stmt = $conn->prepare($sql);
+							$stmt->bindParam(':usuario_id', $_SESSION['user_id']);
+							$stmt->bindParam(':publicacion_id', $_SESSION['id_publicacion']);
+							if ($stmt->execute()){
+									$message = 'Añadido satisfactoriamente';
+							}else {
+								$message = 'Error adoptando mascota';
+							}
+						}
+						?>
+			</tr>
+		</div>
+	</center>
+</body>
+</html>
